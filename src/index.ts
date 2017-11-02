0d46f14aad2360a8a6bbc17ca7264296f2b00f21
徐培堯 <u0124008@nkfust.edu.tw>
2017-11-02 12:04:13 +0800

    ZONE.js已經載入

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import 'reflect-metadata';
-import 'zone.js';
 export { NgRestClientModule } from './ngRestClient.module';
 export { RestClientBuilder } from './restClientBuilder.service';
 export { ApiBase, ApiMethod, ApiParameter, ApiParameterTypes, ApiMethodConfig } from './decorators';
